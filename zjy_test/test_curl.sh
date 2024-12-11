745ff6f3cd6760b9334ebd5b067c6b2975546b5a
tongyan.zjy <tongyan.zjy@alibaba-inc.com>
2024-12-11 14:37:09 +0800

    zhujiayan

--- zjy_test/test_curl.sh
+++ zjy_test/test_curl.sh
@@ -1,9 +1,20 @@
-curl -X POST "http://localhost:8101/generate" \
+#curl -X POST "http://localhost:8101/generate" \
+#-H "Content-Type: application/json" \
+#-d '{"prompt": "<|im_start|>system
+#You are a helpful assistant.
+#<|im_end|>
+#<|im_start|>user
+#你好，Qwen2！请问今天的天气如何
+#<|im_end|>
+#<|im_start|>assistant", "max_tokens": 1000, "temperature": 0.1, "top_p": 0.8}'
+
+
+curl -X POST "http://localhost:8000/v1/chat/completions" \
 -H "Content-Type: application/json" \
--d '{"prompt": "<|im_start|>system
-You are a helpful assistant.
-<|im_end|>
-<|im_start|>user
-你好，Qwen2！请问今天的天气如何
-<|im_end|>
-<|im_start|>assistant", "max_tokens": 1000, "temperature": 0.1, "top_p": 0.8}'
+-d '{"model": "qwen_72b_instruct", "messages": [{"role": "user", "content": "<|im_start|>system \
+You are a helpful assistant. \
+<|im_end|> \
+<|im_start|>user \
+你好，Qwen2！请问今天的天气如何 \
+<|im_end|> \
+<|im_start|>assistant"}],"temperature": 0.1,"top_p": 0.8, "top_k": 1}'
