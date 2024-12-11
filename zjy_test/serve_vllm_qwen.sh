216e3da3d87978d493ff82b551cfe63c2d3f00d2
tongyan.zjy <tongyan.zjy@alibaba-inc.com>
2024-12-11 13:57:23 +0800

    zhujiayan

---
+++ zjy_test/serve_vllm_qwen.sh
@@ -0,0 +1,11 @@
+
+#python serve_vllm_qwen.py
+export CUDA_VISIBLE_DEVICES=6,7
+python -m vllm.entrypoints.openai.api_server --tensor-parallel-size 2 \
+--model "/mnt/tongyan.zjy/openlm/model/Qwen2.5-72B-Instruct-GPTQ-Int4" \
+--served-model-name "qwen_72b_instruct"  \
+--max-model-len 8000 \
+--gpu-memory-utilization 0.7  \
+--dtype=float16  \
+ --enable-prefix-caching \
+ --port 8101
---
+++ zjy_test/test_curl.sh
@@ -0,0 +1,9 @@
+curl -X POST "http://localhost:8101/generate" \
+-H "Content-Type: application/json" \
+-d '{"prompt": "<|im_start|>system
+You are a helpful assistant.
+<|im_end|>
+<|im_start|>user
+你好，Qwen2！请问今天的天气如何
+<|im_end|>
+<|im_start|>assistant", "max_tokens": 1000, "temperature": 0.1, "top_p": 0.8}'
